091d075593cc01c83b9847cd139707836fe4c98f
hogeyman3 <tbrabo98@gmail.com>
2019-06-09 19:39:27 -0400

    Added schtuff.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin
+from flask_login import UserMixin, LoginManager, current_user
 
 
 app = Flask(__name__)
 
-app.config['FLASK_ADMIN_SWATCH'] = 'cosmo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admin.db'
+app.config['SECRET_KEY'] = 'mysecret'
 
+db = SQLAlchemy(app)
 admin = Admin(app, name='myAdmin', template_mode='bootstrap3')
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
 
 @app.route("/")
 def home():
@@ -23,7 +37,18 @@ def contact():
 @app.route('/post')
 def post():
     return render_template("post.html")
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
 
+    return '<h1>Title: {} Subtitle: {} Author: {} Content: {}</h1>'.format(title, subtitle, author, content)
+
+@app.route('/add')
+def add():
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
